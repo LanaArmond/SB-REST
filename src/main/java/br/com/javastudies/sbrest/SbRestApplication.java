0f73c3fb8a9de5933c2bcb6d755a510a6aa60b5c
LanaArmond <anabialana@gmail.com>
2026-03-30 12:34:54 -0300

    initial commit

---
+++ src/main/java/br/com/javastudies/sbrest/SbRestApplication.java
@@ -0,0 +1,13 @@
+package br.com.javastudies.sbrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbRestApplication.class, args);
+    }
+
+}
