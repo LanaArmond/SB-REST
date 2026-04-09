55ea9ea88cd1a86c4451cc6d11ed0abc5e2a506a
LanaArmond <anabialana@gmail.com>
2026-04-09 11:38:28 -0300

    CORS+Tests

--- src/main/java/br/com/javastudies/sbrest/config/WebConfig.java
+++ src/main/java/br/com/javastudies/sbrest/config/WebConfig.java
@@ -1,13 +1,31 @@
 package br.com.javastudies.sbrest.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    // Configuração de CORS
+
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns = "";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatterns.split(",");
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                // .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedMethods("*")
+                .allowCredentials(true);
+    }
+
+    // Configuração de tipos de arquivos
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         // Via EXTENSION. _.xml or _.json Deprecated on Spring Boot 2.6
--- src/test/java/br/com/javastudies/sbrest/config/TestConfigs.java
+++ src/test/java/br/com/javastudies/sbrest/config/TestConfigs.java
@@ -2,4 +2,11 @@ package br.com.javastudies.sbrest.config;
 
 public interface TestConfigs {
     int SERVER_PORT = 8888;
+
+    String HEADER_PARAM_AUTHORIZATION = "Authorization";
+    String HEADER_PARAM_ORIGIN = "Origin";
+
+    String ORIGIN_LOCAL = "http://localhost:8080";
+    String ORIGIN_ERUDIO = "https://www.erudio.com.br";   // Funciona
+    String ORIGIN_SEMERU = "https://www.semeru.com.br";   // Não funciona
 }
